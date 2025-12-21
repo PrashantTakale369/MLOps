573870361b2bead8bcb8a95df2d2d9c76f6dbe13
Prashant Takale <147067738+PrashantTakale369@users.noreply.github.com>
2025-12-21 23:22:13 +0530

    Done

---
+++ Py_Oops/class_and_obj.py
@@ -0,0 +1,48 @@
+
+# introduction to class and object in python
+
+class employee:
+    
+    def __init__(self):   # < ---- special function - constructor  // This is called Parameterized Constructor
+        self.id = 123
+        self.name = "Prashant Takale"
+        self.salary = 50000000
+        self.designation = "SDE"
+    
+    # creating a function inside class
+    def travel(sel , name):
+        print(f"hi my name is {name}")
+
+
+ #object creation 
+sam = employee()  
+# print(sam.designation) 
+
+
+
+sam.travel("Prashant Takale")   # < --- obj to fun calling
+
+
+
+"""
+
+1 > What is __init__ in Python
+-- >  __init__ is a special method that runs automatically when an object is created.
+-- > if we not write (__init__) then Python creates a default constructor for you.
+
+2 > Why is self used
+-- > self means current object.
+-- > Without self, Python won't know which object's data you are talking about
+
+3 > self.id , self.name , ..... ?
+-- > They are called instance variables
+-- > Because each object will have its own copy
+
+4 > Where do you use constructors in real projects?
+-- >To initialize:
+                    1. user details
+                    2. database connections
+                    3. model parameters
+                    4. employee data
+                    ok : 
+"""
---
+++ Py_Oops/questions_ans.py
@@ -0,0 +1,44 @@
+"""
+
+
+1 > Why is Python called an OOPS language?
+
+-- > Because in Python EVERYTHING is an object
+
+    -- >"hello" >  data
+        .upper() > function (method)
+        Data + Method = Object
+
+-- > Python is an object-oriented language because everything in Python is treated as 
+     an object and it supports all OOPS concepts like 
+     1. class, 
+     2. object,    
+        3. inheritance, 
+        4. encapsulation, 
+        4. polymorphism,
+        5. abstraction.
+"""
+
+"""
+
+
+2 > Advantages of OOPS
+
+-- > OOPS improves reusability, security, maintainability, scalability, and overall code quality.
+
+-- > OOPS provides better data security using encapsulation and data hiding, ensuring controlled access to data. 
+-- > It allows flexibility and scalability through polymorphism, making it easy to modify or add new features without
+    affecting existing code. 
+-- > OOPS also enables developers to create their own custom data types using classes, which helps in modeling 
+    real-world problems more effectively.
+
+    
+    -- > you can creat your own datatype 
+    -- > like a = x ,  b = y 
+    -- > then print( a + b) --- > return the  xy 
+
+    but logically we want x + y   --- > so we can creat oue owe datatype : 
+
+
+    
+"""
